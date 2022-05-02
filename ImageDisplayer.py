93e0f27cbba07edee87ececc2a1c411a6d715891
Suthapalli.Akhil <Suthapalli.Akhil@tvsmotor.com>
2022-05-02 09:46:25 +0530

    Added new images

--- ImageDisplayer.py
+++ ImageDisplayer.py
@@ -25,6 +25,8 @@ imgA2 = Image.open("A2.png")
 imgA3 = Image.open("A3.png")
 imgA4 = Image.open("A4.png")
 imgA5 = Image.open("A5.png")
+imgA6 = Image.open("A6.png")
+imgA7 = Image.open("A7.png")
 
 def exit_fun(event):
     global running
@@ -33,7 +35,7 @@ def exit_fun(event):
     root.destroy()
 
 def loop():
-    global label,img 
+    global label,img
     i=1
     while(running):
         time.sleep(1)
@@ -44,44 +46,52 @@ def loop():
             print("exception")
             return
         i = i+1
-        if i==5:
+        if i==7:
             i=0
-            
-        
-        
-        
+
+
+
+
 def load_images():
     global img, rotate
     img = []
     imgB1 = imgA1.resize((width,height))
     imgC1 = imgB1.rotate(angle=rotate)
     img1 = ImageTk.PhotoImage(imgC1)
-    
+
     imgB2 = imgA2.resize((width,height))
     imgC2 = imgB2.rotate(angle=rotate)
     img2 = ImageTk.PhotoImage(imgC2)
-    
+
     imgB3 = imgA3.resize((width,height))
     imgC3 = imgB3.rotate(angle=rotate)
     img3 = ImageTk.PhotoImage(imgC3)
-    
+
     imgB4 = imgA4.resize((width,height))
     imgC4 = imgB4.rotate(angle=rotate)
     img4 = ImageTk.PhotoImage(imgC4)
-    
+
     imgB5 = imgA5.resize((width,height))
     imgC5 = imgB5.rotate(angle=rotate)
     img5 = ImageTk.PhotoImage(imgC5)
-    
-    img = [img1 , img2, img3, img4, img5]
+
+    imgB6 = imgA6.resize((width,height))
+    imgC6 = imgB6.rotate(angle=rotate)
+    img6 = ImageTk.PhotoImage(imgC6)
+
+    imgB7 = imgA7.resize((width,height))
+    imgC7 = imgB7.rotate(angle=rotate)
+    img7 = ImageTk.PhotoImage(imgC7)
+
+    img = [img1 , img2, img3, img4, img5, img6, img7]
 
 def rotate_fun(event):
     global rotate
-    
+
     rotate = rotate + 90
     if rotate == 360:
         rotate = 0
-    
+
     print("rotating to ",rotate)
     load_images()
 
@@ -91,14 +101,14 @@ if __name__ == "__main__" :
     root.attributes('-fullscreen', True)
     root.bind("<Button-1>",exit_fun)
     root.bind("<Button-3>",rotate_fun)
-    
+
     width = root.winfo_screenwidth()
     height = root.winfo_screenheight()
     load_images()
     #change all images
     imgtemp = load_image_temp.resize((width,height))
     load_image = ImageTk.PhotoImage(imgtemp)
-    
+
     label = Label(root,image = load_image)
     label.pack(fill = BOTH, expand = YES)
     threading.Thread(target = loop).start()
